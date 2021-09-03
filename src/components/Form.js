6b085b6b92332a309fbae8b0aee81f611d43aeb7
nijimajohn <hirokikotera0211@gmail.com>
2021-09-03 09:40:35 +0900

    [React_weather-check-applickation-withAPI]

---
+++ src/components/Form.js
@@ -0,0 +1,16 @@
+
+
+
+
+const Form = (props) => {
+
+  return (
+    <form>
+      < input type=" text" name=" city" placeholder=" 都市 名" onChange={e => props.setCity(e.target.value)} />
+
+      <button type="submit" onClick={props.getWeather}>Get Weather</button>
+    </form>
+  );
+};
+
+export default Form;
